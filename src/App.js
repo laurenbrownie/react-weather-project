33005e7fb00449c43fd9bb389dba58cb3730ef9e
laurenbrownie <95157266+laurenbrownie@users.noreply.github.com>
2022-02-03 08:45:15 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lauren Brown and is{" "}
-        <a
-          href="https://github.com/laurenbrownie/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Lauren Brown and is{" "}
+          <a
+            href="https://github.com/laurenbrownie/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
