efd903d1029616f10ee8d56a1d5bd7c9dbbb9bd4
laurenbrownie <95157266+laurenbrownie@users.noreply.github.com>
2022-02-03 08:17:46 -0500

    link to source code

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <a
+        href="https://github.com/laurenbrownie/react-weather-project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        View Source Code
+      </a>
+    </div>
+  );
 }
