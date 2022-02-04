6be92e12001b9874fb9b928f99a2461360a52a95
laurenbrownie <95157266+laurenbrownie@users.noreply.github.com>
2022-02-04 10:03:59 -0600

    basic styling

---
+++ src/Weather.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Paris</h1>
+      <ul>
+        <li>Thursday 9:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="Cloudy"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>precipitation: 2%</li>
+            <li>humidity: 77%</li>
+            <li>wind: 10km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
