0208be6a8ada9d8fc1915848598341dd8f8b6ba6
laurenbrownie <95157266+laurenbrownie@users.noreply.github.com>
2022-02-04 13:12:35 -0600

    slight design mod

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ export default function App() {
       <div className="container">
         <Weather city="Des Moines" />
         <footer>
-          This project was coded by Lauren Brown and is{" "}
+          coded by Lauren Brown 👩🏼‍💻{" "}
           <a
             href="https://github.com/laurenbrownie/react-weather-project"
             target="_blank"
             rel="noreferrer"
           >
-            open-sourced
+            view source code
+          </a>{" "}
+          💜 hosted on{" "}
+          <a href="https://netlify.com" target="blank" rel="noreferrer">
+            netlify
           </a>
-          .
         </footer>
       </div>
     </div>
