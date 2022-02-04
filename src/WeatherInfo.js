00f9afa9a5002a716783681ae3119a487636d20a
laurenbrownie <95157266+laurenbrownie@users.noreply.github.com>
2022-02-04 13:03:25 -0600

    unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodaysDate from "./TodaysDate";
+import TemperatureInfo from "./TemperatureInfo";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,10 +15,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.description} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <TemperatureInfo celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
